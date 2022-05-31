d95541a27bcaeb97246b8856a19299df8455f6a6
daniel frickstein <102364238+frickstein@users.noreply.github.com>
2022-05-31 13:29:41 +0200

    == juggling stuff

--- aufgaben/L09/migrane.js
+++ aufgaben/L09/migrane.js
@@ -1,6 +1,16 @@
 var L09;
 (function (L09) {
     window.addEventListener("load", Clicker);
+    var chain = [
+        "../L09/assets/Drumpad/kick.mp3",
+        "../L09/assets/Drumpad/snare.mp3",
+        "../L09/assets/Drumpad/hihat.mp3"
+    ];
+    var counter = 0;
+    var playing;
+    var z;
+    var num = 0;
+    var rng = 0;
     function Clicker() {
         document.querySelector(".pad1").addEventListener("click", function () { playSample("../L09/assets/Drumpad/kick.mp3"); });
         document.querySelector(".pad2").addEventListener("click", function () { playSample("../L09/assets/Drumpad/snare.mp3"); });
@@ -11,31 +21,22 @@ var L09;
         document.querySelector(".pad7").addEventListener("click", function () { playSample("../L09/assets/Drumpad/C.mp3"); });
         document.querySelector(".pad8").addEventListener("click", function () { playSample("../L09/assets/Drumpad/laugh-1.mp3"); });
         document.querySelector(".pad9").addEventListener("click", function () { playSample("../L09/assets/Drumpad/laugh-2.mp3"); });
-        document.querySelector(".playBTN").addEventListener("click", function () { playButton(); });
-        document.querySelector(".playRNG").addEventListener("click", function () { Randomizer(); });
+        document.querySelector(".fa-play").addEventListener("click", function () { playButton(); });
+        document.querySelector(".fa-random").addEventListener("click", function () { remixer(); });
     }
-    var counter = 0;
-    var playing;
-    var x;
-    var num = 0;
-    var rng = 0;
-    var chain = [
-        "../L08/assets/Drumpad/kick.mp3",
-        "../L08/assets/Drumpad/snare.mp3",
-        "../L08/assets/Drumpad/hihat.mp3"
-    ];
-    //var toggle: HTMLElement = document.querySelector("#toggler");
     function playSample(beats) {
         var sound = new Audio(beats);
+        sound.volume = .25;
         sound.play();
     }
-    function Randomizer() {
-        num = setInterval(Mixer, 500);
-        function Mixer() {
-            for (var i = 0; i <= chain.length; i++)
-                ;
-            rng = Math.floor(Math.random() * 3);
-            playing = true;
+    function remixer() {
+        num = setInterval(StartMixer, 600);
+        function StartMixer() {
+            for (var i = 0; i <= chain.length; i++) {
+                rng = Math.floor(Math.random() * 3);
+                playSample(chain[rng]);
+                playing = true;
+            }
         }
     }
     function playMixer() {
@@ -52,19 +53,19 @@ var L09;
         document.querySelector("#toggler").classList.remove("fa-play");
         playMixer();
         playing = true;
-        console.log("playing sound");
+        console.log("sound playing");
         if (document.querySelector("#toggler").getAttribute("class") == " fas fa-stop fa-8x") {
             document.querySelector("#toggler").setAttribute("class", "fas fa-play fa-8x");
-            document.querySelector("#toggler").classList.remove("fa-stop");
-            ResetMixer();
-            clearInterval(0);
+            clearInterval(num);
             playing = false;
-            console.log("sound stoped");
+            resetMixer();
+            console.log("sound stopped");
         }
     }
-    function ResetMixer() {
+    function resetMixer() {
         chain.length = 0;
         chain.length = 3;
+        playing = false;
     }
 })(L09 || (L09 = {}));
 //# sourceMappingURL=migrane.js.map
--- aufgaben/L09/migrane.ts
+++ aufgaben/L09/migrane.ts
@@ -1,6 +1,18 @@
 namespace L09 {
     window.addEventListener("load", Clicker);
 
+    var chain: string [] = [
+        "../L09/assets/Drumpad/kick.mp3",
+        "../L09/assets/Drumpad/snare.mp3",
+        "../L09/assets/Drumpad/hihat.mp3"
+    ];
+    
+    var counter: number = 0;
+    var playing: boolean;
+    var z: number;
+    var num: number = 0;
+    var rng: number = 0;
+
     function Clicker(): void {
         document.querySelector(".pad1").addEventListener("click", function (): void { playSample("../L09/assets/Drumpad/kick.mp3"); });
         document.querySelector(".pad2").addEventListener("click", function (): void { playSample("../L09/assets/Drumpad/snare.mp3"); });
@@ -11,77 +23,62 @@ namespace L09 {
         document.querySelector(".pad7").addEventListener("click", function (): void { playSample("../L09/assets/Drumpad/C.mp3"); });
         document.querySelector(".pad8").addEventListener("click", function (): void { playSample("../L09/assets/Drumpad/laugh-1.mp3"); });
         document.querySelector(".pad9").addEventListener("click", function (): void { playSample("../L09/assets/Drumpad/laugh-2.mp3"); });
-        document.querySelector(".playBTN").addEventListener("click", function (): void {playButton(); });
-        document.querySelector(".playRNG").addEventListener("click", function(): void {Randomizer(); });
+        document.querySelector(".fa-play").addEventListener("click", function (): void {playButton(); });
+        document.querySelector(".fa-random").addEventListener("click", function(): void {remixer(); });
    
     }
 
-    var counter: number = 0;
-    var playing: boolean;
-    var x: number;
-    var num: number = 0;
-    var rng: number = 0;
-
-
-    
-    var chain: string[] = [
-        "../L08/assets/Drumpad/kick.mp3",
-        "../L08/assets/Drumpad/snare.mp3",
-        "../L08/assets/Drumpad/hihat.mp3"
-    ];
-
-        //var toggle: HTMLElement = document.querySelector("#toggler");
-
-    function playSample(beats: string): void {
-        var sound: HTMLAudioElement = new Audio(beats);
+    function playSample(beats: string): void { 
+        var sound: HTMLAudioElement = new Audio(beats);  
+        sound.volume = .25; 
         sound.play();
-
     }
-
-    function Randomizer(): void {       
-    num = setInterval(Mixer, 500);
-
-    function Mixer(): void {
-        for (var i: number = 0; i <= chain.length; i++);
-        rng = Math.floor(Math.random() * 3);
-        playing = true;
+    
+    
+    
+    function remixer(): void {
+        num = setInterval(StartMixer, 600);
+    
+        function StartMixer(): void {
+            for (var i: number = 0; i <= chain.length; i++) {
+                rng = Math.floor(Math.random() * 3);
+                playSample(chain[rng]);
+                playing = true;
+            }
+        }
     }
-    }    
     
-    function playMixer(): void {           
+    function playMixer(): void { 
+            setInterval(function(): void {   
+                                
+                    playSample(chain[counter]);
+                    counter++;
+                    if (counter == 3) {
+                        counter = 0;         
+                    }   
+            },          600);
+    }  
     
-        setInterval(function(): void {                       
-            playSample(chain[counter]);
-            counter++;
-            if (counter == 3) {
-                counter = 0;
-            }            
-        },          600);
-
-
-}
- 
     function playButton(): void { 
-    document.querySelector("#toggler").setAttribute("class", "fas fa-stop fa-8x");
-    document.querySelector("#toggler").classList.remove("fa-play");
-    playMixer();
-    playing = true;
-    console.log("playing sound");
-
-    if (document.querySelector("#toggler").getAttribute("class") == " fas fa-stop fa-8x") {
-        document.querySelector("#toggler").setAttribute("class", "fas fa-play fa-8x");
-        document.querySelector("#toggler").classList.remove("fa-stop");
-        ResetMixer();
-        clearInterval(0);
-        playing = false;
-        console.log("sound stoped");
+            document.querySelector("#toggler").setAttribute("class", "fas fa-stop fa-8x");
+            document.querySelector("#toggler").classList.remove("fa-play");
+            playMixer();
+            playing = true;
+            console.log("sound playing");
+    
+            if (document.querySelector("#toggler").getAttribute("class") == " fas fa-stop fa-8x") {
+                document.querySelector("#toggler").setAttribute("class", "fas fa-play fa-8x");
+                clearInterval(num);
+                playing = false;
+                resetMixer();
+                console.log("sound stopped");
+            }
     }
-}
-
-
-    function ResetMixer(): void {
-    chain.length = 0;
-    chain.length = 3;
-}
+    
+    function resetMixer(): void {
+        chain.length = 0;
+        chain.length = 3;
+        playing = false;
+    }
 
 }
