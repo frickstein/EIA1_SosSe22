a8458a13d3022cdba7e33a98c4fc4775268390d2
daniel frickstein <102364238+frickstein@users.noreply.github.com>
2022-06-30 14:25:07 +0200

    ++

---
+++ aufgaben/L11/spaghetti.js
@@ -0,0 +1,91 @@
+//objekte festlegen
+const sentences = [
+    //0
+    {
+        german: "Das ist leicht",
+        spanish: ["Eso", "es", "fácil"]
+    },
+    //1
+    {
+        german: "Spanisch ist meine erste Fremdsprache",
+        spanish: ["el", "español", "es", "mi", "primera", "lengua", "extranjera"]
+    },
+    //2    
+    {
+        german: "Könnten sie das wiederholen?",
+        spanish: ["Podrías", "repetir", "eso", "por", "favor"]
+    },
+    //3    
+    {
+        german: "Ich fahre morgen nach Mexiko",
+        spanish: ["me", "voy", "a", "mexico", "mañana"]
+    },
+    //4
+    {
+        german: "Ich komme ursprünglich aus Deutschland",
+        spanish: ["soy", "originario", "de", "alemania"]
+    },
+    //5    
+    {
+        german: "Wann kommt der Bus?",
+        spanish: ["Cuándo", "viene", "el", "autobús"]
+    },
+    //6    
+    {
+        german: "Mutter Maria, was ist das den?",
+        spanish: ["Madre", "María", "qué", "es", "eso"]
+    },
+    //7    
+    {
+        german: "Mutter Maria, was ist das den?",
+        spanish: ["Madre", "María", "qué", "es", "eso"]
+    },
+    //8    
+    {
+        german: "Mutter Maria, was ist das den?",
+        spanish: ["Madre", "María", "qué", "es", "eso"]
+    },
+    //9    
+    {
+        german: "Mutter Maria, was ist das den?",
+        spanish: ["Madre", "María", "qué", "es", "eso"]
+    },
+    //10    
+    {
+        german: "Mutter Maria, was ist das den?",
+        spanish: ["Madre", "María", "qué", "es", "eso"]
+    },
+    //11    
+    {
+        german: "Mutter Maria, was ist das den?",
+        spanish: ["Madre", "María", "qué", "es", "eso"]
+    },
+    //12   
+    {
+        german: "Mutter Maria, was ist das den?",
+        spanish: ["Madre", "María", "qué", "es", "eso"]
+    },
+    //13 
+    {
+        german: "Mutter Maria, was ist das den?",
+        spanish: ["Madre", "María", "qué", "es", "eso"]
+    },
+    //14  
+    {
+        german: "Mutter Maria, was ist das den?",
+        spanish: ["Madre", "María", "qué", "es", "eso"]
+    },
+    //15  
+    {
+        german: "Mutter Maria, was ist das den?",
+        spanish: ["Madre", "María", "qué", "es", "eso"]
+    }
+];
+// Funktion & co. für die zufälligen Sätze
+function randomSentence() {
+    for (var i = 0; i < sentences.length; i++) {
+        const element = sentences[i];
+    }
+}
+;
+//# sourceMappingURL=spaghetti.js.map
