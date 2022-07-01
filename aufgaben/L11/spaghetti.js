b4b60c5413b175ffb8e31a2164cb9afc68d3e236
daniel frickstein <102364238+frickstein@users.noreply.github.com>
2022-07-01 21:53:14 +0200

    ++

--- aufgaben/L11/spaghetti.js
+++ aufgaben/L11/spaghetti.js
@@ -1,9 +1,13 @@
-window.addEventListener("load", Injector);
+// as a heads up: i have actively decided to ignore certain coding requirements set by tslint for my own visual clarity, 
+// as well as thinking that (double) declaring every variables/ functions according to tslint to be obsolete.
+window.addEventListener("load", function () {
+    DiffSelect();
+});
 const sentences = [
     //0
     {
         german: "Das ist leicht",
-        spanish: ["Eso", "es", "fácil"]
+        spanish: ["eso", "es", "fácil"]
     },
     //1
     {
@@ -13,7 +17,7 @@ const sentences = [
     //2    
     {
         german: "Könnten sie das wiederholen?",
-        spanish: ["Podrías", "repetir", "eso", "por", "favor"]
+        spanish: ["podrías", "repetir", "eso", "por", "favor"]
     },
     //3    
     {
@@ -28,7 +32,7 @@ const sentences = [
     //5    
     {
         german: "Wann kommt der Bus?",
-        spanish: ["Cuándo", "viene", "el", "autobús"]
+        spanish: ["cuándo", "viene", "el", "autobús"]
     },
     //6    
     {
@@ -37,71 +41,77 @@ const sentences = [
     },
     //7    
     {
-        german: "Mutter Maria, was ist das den?",
-        spanish: ["Madre", "María", "qué", "es", "eso"]
+        german: "Wie lange bleibst du?",
+        spanish: ["cuánto", "tiempo", "te", "quedas"]
     },
     //8    
     {
-        german: "Mutter Maria, was ist das den?",
-        spanish: ["Madre", "María", "qué", "es", "eso"]
+        german: "Ich hoffe, dass das wasser nicht kalt ist.",
+        spanish: ["espero", "que", "el", "agua", "no", "este", "fria"]
     },
     //9    
     {
-        german: "Mutter Maria, was ist das den?",
-        spanish: ["Madre", "María", "qué", "es", "eso"]
+        german: "Ich freue mich, dich kennenzulernen!",
+        spanish: ["encantada", "de", "conocerte"]
     },
     //10    
     {
-        german: "Mutter Maria, was ist das den?",
-        spanish: ["Madre", "María", "qué", "es", "eso"]
+        german: "Entschuldige, wie komme ich den hierher?",
+        spanish: ["disculpe", "cómo", "llego", "aquí"]
     },
     //11    
     {
-        german: "Mutter Maria, was ist das den?",
-        spanish: ["Madre", "María", "qué", "es", "eso"]
+        german: "Tut mir leid, aber das ist mir zu teuer.",
+        spanish: ["lo", "siento", "pero", "eso", "es", "demasiado", "caro", "para", "mí."]
     },
     //12   
     {
-        german: "Mutter Maria, was ist das den?",
-        spanish: ["Madre", "María", "qué", "es", "eso"]
+        german: "Dieser Satz wurde in spanische übersetzt.",
+        spanish: ["esta", "frase", "ha", "sido", "traducida", "al", "español"]
     },
     //13 
     {
-        german: "Mutter Maria, was ist das den?",
-        spanish: ["Madre", "María", "qué", "es", "eso"]
+        german: "Mir gehen echt die Ideen aus.",
+        spanish: ["realmente", "me", "estoy", "quedando", "sin", "ideas."]
     },
     //14  
     {
-        german: "Mutter Maria, was ist das den?",
-        spanish: ["Madre", "María", "qué", "es", "eso"]
+        german: "Meine Aufmerksamkeit ist komplett durchgebrannt.",
+        spanish: ["mi", "atención", "está", "completamente", "impresionada"]
     },
     //15  
     {
-        german: "Mutter Maria, was ist das den?",
-        spanish: ["Madre", "María", "qué", "es", "eso"]
+        german: "Eigentlich ist Englisch meine erste Fremdsprache.",
+        spanish: ["el", "inglés", "es", "en", "realidad", "mi", "primer", "idioma", "extranjero."]
     }
 ];
+//variables later purposes
 var selected;
-var selectWord = []; // das sollte die Variable mit einem leeren Array gleichstellen
+var selectWord = []; // this empty bracket thing should equate the variable to an empty array
 var previousSentences = [];
 var countTracker = 0;
 var sentenceTracker = 0;
-// folgende Funktion erzeugt eine zufällige Zahl und sendet diese später an die richtige Stelle
+var diffTracker = 0;
+var easyDiff = document.querySelector(".easy");
+var midDiff = document.querySelector(".mid");
+var hardDiff = document.querySelector(".hard");
+// the following function generates a random number and later on sends it to correct location
 function RandomSentence() {
     var max = 14;
     var random = Math.floor(Math.random() * (max + 1));
-    while (previousSentences.includes(random)) {
+    while (previousSentences.includes(random)) { //keeps picking a random number that hasnt been selected yet
         random = Math.floor(Math.random() * (max + 1));
     }
-    previousSentences.push(random); //keeps track of the number
+    previousSentences.push(random); //keeps track of the picked numbers
     return sentences[random];
 }
+// following function randomizes the order of the spanish words
 function RandomSpanish(spanish) {
-    let newSpanish = [];
-    for (let i = 0; i < spanish.length; i++) { //creates a copy of "spanish" and its newSpanish
+    let newSpanish = []; // a copy of the original 'spanish'; its to avoid updating the original one 
+    for (let i = 0; i < spanish.length; i++) {
         newSpanish[i] = spanish[i];
     }
-    for (let i = newSpanish.length - 1; i > 0; i--) { //swaps items around randomly based on the Fisher–Yates shuffle
+    for (let i = newSpanish.length - 1; i > 0; i--) { // swaps items around randomly based on the Fisher–Yates shuffle
         var rdm = Math.floor(Math.random() * (i + 1));
         var old = newSpanish[i];
         newSpanish[i] = newSpanish[rdm];
@@ -111,35 +121,31 @@ function RandomSpanish(spanish) {
 }
 function Injector() {
     selected = RandomSentence();
-    document.querySelector(".head").innerHTML = selected.german;
-    console.log(selected);
-    var gridContainer = document.querySelector(".grid-container");
+    document.querySelector(".point-counter").innerHTML = countTracker.toString() + " Pts."; // showing of the total points in html
+    document.querySelector(".head").innerHTML = " \" " + selected.german + " \" "; // showing off the germans sentence
+    var gridContainer = document.querySelector(".grid-container"); // variables for convienience
     var inputBox = document.querySelector(".input-box");
     var rngSpanish = RandomSpanish(selected.spanish);
-    gridContainer.innerHTML = ""; //entfernt alle elemete aus dem Grid 
-    inputBox.innerHTML = ""; // entfernt alle Übersetzungen
-    selectWord = []; // leert die Worterkennung für den nächsten Satz
-    for (let index = 0; index < rngSpanish.length; index++) {
+    gridContainer.innerHTML = ""; // removes all elements from the grid
+    inputBox.innerHTML = ""; // removes all translated words
+    selectWord = []; // clears the tracker for the next sentence
+    for (let index = 0; index < rngSpanish.length; index++) { //creating the 'spanish' as grid-items in the grid-container 
         let element = rngSpanish[index];
         var item = document.createElement("div");
         item.setAttribute("class", "grid-item");
         item.innerHTML = element;
         item.addEventListener("click", function () { OrderChecker(element); });
-        gridContainer.appendChild(item);
+        gridContainer.appendChild(item); //adds 'item' as a child to grid-container -> puts grid-item into grid-container
     }
 }
+// index for the propper spanish list of words that gets checked
+// said words will ne displayed in the html element based on the numerical value within the array
 function OrderChecker(word) {
-    // index für die korrekte spanische Wort-liste, die gecheckt wird
-    // die Wörter werden durch deren numerischen Wert im Array in die input-box im html ausgespuckt
     var nextSpanish = selectWord.length;
-    console.log(nextSpanish);
-    console.log(word);
-    console.log(selected.spanish[nextSpanish]);
-    console.log(selected);
-    if (selected.spanish[nextSpanish] == word) {
+    if (selected.spanish[nextSpanish] == word) { //its checking if the clicked word is the correct one
         var input = document.querySelector(".input-box");
-        input.innerHTML = input.innerHTML + " " + word; //das ist dazu da, nach der richtigen antwort nochmal ein abstand einzubringe
-        selectWord.push(word); //Das ist da um die Auswahl bzw die reihnfolge zu merken
+        input.innerHTML = input.innerHTML + " " + word; // this is here to put some space inbetween the correct answers
+        selectWord.push(word); // this part is there to remember the order of the selection
         countTracker++;
     }
     else {
@@ -150,12 +156,9 @@ function OrderChecker(word) {
         countTracker = 0;
     }
     document.querySelector(".point-counter").innerHTML = countTracker.toString() + " Pts.";
-    var easyDiff = document.querySelector(".easy");
-    var midDiff = document.querySelector(".mid");
-    var hardDiff = document.querySelector(".hard");
-    if (selectWord.length == selected.spanish.length) { //= 
+    if (selectWord.length == selected.spanish.length) { // if the amount of correct words coressponds with the total length, the sentences completed
         sentenceTracker++;
-        if (sentenceTracker == 5) {
+        if (sentenceTracker == 5 && diffTracker == 0) { // if the amount of completed sentences & difficulty align with the condition, a confirm prompt will ask to continue
             if (confirm("Glückwunsch! Die Punktzahl: " + countTracker + ". Du hast den ersten Kurs geschafft! Möchtest du fortfahren?")) {
                 midDiff.setAttribute("class", "diff mid current");
                 easyDiff.setAttribute("class", "diff easy");
@@ -163,7 +166,7 @@ function OrderChecker(word) {
                 Injector();
             }
         }
-        else if (sentenceTracker == 10) {
+        else if (sentenceTracker == 10 && diffTracker == 1) {
             if (confirm("Glückwunsch! Die Punktzahl: " + countTracker + ". Du hast den zweiten Kurs geschafft! Möchtest du fortfahren?")) {
                 hardDiff.setAttribute("class", "diff hard current");
                 midDiff.setAttribute("class", "diff mid");
@@ -172,12 +175,44 @@ function OrderChecker(word) {
             }
         }
         else if (sentenceTracker == 15) {
-            alert("Glückwunsch! Die Punktzahl: " + countTracker + ". Du hast den ganzen Kurs geschafft!");
+            alert("Glückwunsch! Die Punktzahl: " + countTracker + ". Du hast den dritten Kurs geschafft!");
             hardDiff.setAttribute("class", "diff hard current");
         }
         else {
             Injector();
         }
     }
+}
+// a function to enable difficulty selection
+function DiffSelect() {
+    easyDiff = document.querySelector(".easy");
+    midDiff = document.querySelector(".mid");
+    hardDiff = document.querySelector(".hard");
+    easyDiff.addEventListener("click", function () { SetDiff(0); });
+    midDiff.addEventListener("click", function () { SetDiff(1); });
+    hardDiff.addEventListener("click", function () { SetDiff(2); });
+}
+function SetDiff(track) {
+    diffTracker = track;
+    previousSentences = [];
+    sentenceTracker = 0;
+    countTracker = 0;
+    if (track == 0) { // adjusting styling after selection
+        easyDiff.setAttribute("class", "diff easy current");
+        midDiff.setAttribute("class", "diff mid");
+        hardDiff.setAttribute("class", "diff hard");
+    }
+    else if (track == 1) {
+        easyDiff.setAttribute("class", "diff easy");
+        midDiff.setAttribute("class", "diff mid current");
+        hardDiff.setAttribute("class", "diff hard");
+    }
+    else if (track == 2) {
+        easyDiff.setAttribute("class", "diff easy");
+        midDiff.setAttribute("class", "diff mid");
+        hardDiff.setAttribute("class", "diff hard current");
+    }
+    Injector();
+    document.querySelector(".point-counter").innerHTML = countTracker.toString() + " Pts.";
 }
 //# sourceMappingURL=spaghetti.js.map
--- aufgaben/L11/spaghetti.ts
+++ aufgaben/L11/spaghetti.ts
@@ -1,10 +1,15 @@
+// as a heads up: i have actively decided to ignore certain coding requirements set by tslint for my own visual clarity, 
+// as well as thinking that (double) declaring every variables/ functions according to tslint to be obsolete.
 
-window.addEventListener("load", Injector);
+window.addEventListener("load", function() {
+ DiffSelect();
+ }
+);
 
-//objekte festlegen
+//defining objects
 interface Basis {
-    german:  string;
-    spanish: string[];
+ german:  string;
+ spanish: string[];
 }
 
 
@@ -13,7 +18,7 @@ const sentences: Basis[] = [
     //0
     {
     german: "Das ist leicht", 
-    spanish: ["Eso", "es", "fácil"]
+    spanish: ["eso", "es", "fácil"]
     },
 
     //1
@@ -25,7 +30,7 @@ const sentences: Basis[] = [
     //2    
     {
     german: "Könnten sie das wiederholen?", 
-    spanish: ["Podrías", "repetir", "eso", "por", "favor"]   
+    spanish: ["podrías", "repetir", "eso", "por", "favor"]   
     },
 
     //3    
@@ -43,7 +48,7 @@ const sentences: Basis[] = [
     //5    
     {
     german: "Wann kommt der Bus?", 
-    spanish: ["Cuándo", "viene", "el", "autobús"]
+    spanish: ["cuándo", "viene", "el", "autobús"]
     },
 
     //6    
@@ -54,185 +59,235 @@ const sentences: Basis[] = [
     
     //7    
     {
-    german: "Mutter Maria, was ist das den?", 
-    spanish: ["Madre", "María", "qué", "es", "eso"]
+    german: "Wie lange bleibst du?", 
+    spanish: ["cuánto", "tiempo", "te", "quedas"]
     },    
     
     //8    
     {
-    german: "Mutter Maria, was ist das den?", 
-    spanish: ["Madre", "María", "qué", "es", "eso"]
+    german: "Ich hoffe, dass das wasser nicht kalt ist.", 
+    spanish: ["espero", "que", "el", "agua", "no", "este", "fria"]
     },
 
     //9    
     {
-    german: "Mutter Maria, was ist das den?", 
-    spanish: ["Madre", "María", "qué", "es", "eso"]
+    german: "Ich freue mich, dich kennenzulernen!", 
+    spanish: ["encantada", "de", "conocerte"]
     },
 
     //10    
     {
-    german: "Mutter Maria, was ist das den?", 
-    spanish: ["Madre", "María", "qué", "es", "eso"]
+    german: "Entschuldige, wie komme ich den hierher?", 
+    spanish: ["disculpe", "cómo", "llego", "aquí"]
     },
 
     //11    
     {
-    german: "Mutter Maria, was ist das den?", 
-    spanish: ["Madre", "María", "qué", "es", "eso"]
+    german: "Tut mir leid, aber das ist mir zu teuer.", 
+    spanish: ["lo", "siento", "pero", "eso", "es", "demasiado", "caro", "para", "mí."]
     },
 
     //12   
     {
-    german: "Mutter Maria, was ist das den?", 
-    spanish: ["Madre", "María", "qué", "es", "eso"]
+    german: "Dieser Satz wurde in spanische übersetzt.", 
+    spanish: ["esta", "frase", "ha", "sido", "traducida", "al", "español"]
     },  
 
     //13 
     {
-    german: "Mutter Maria, was ist das den?", 
-    spanish: ["Madre", "María", "qué", "es", "eso"]
+    german: "Mir gehen echt die Ideen aus.", 
+    spanish: ["realmente", "me", "estoy", "quedando", "sin", "ideas."]
     },   
 
     //14  
     {
-    german: "Mutter Maria, was ist das den?", 
-    spanish: ["Madre", "María", "qué", "es", "eso"]
+    german: "Meine Aufmerksamkeit ist komplett durchgebrannt.", 
+    spanish: ["mi", "atención", "está", "completamente", "impresionada"]
     },  
 
     //15  
     {
-    german: "Mutter Maria, was ist das den?", 
-    spanish: ["Madre", "María", "qué", "es", "eso"]
+    german: "Eigentlich ist Englisch meine erste Fremdsprache.", 
+    spanish: ["el", "inglés", "es", "en", "realidad", "mi", "primer", "idioma", "extranjero."]
     }
 ];
 
-//variablen für später
-var selected: Basis;
-var selectWord: string[] = []; // das sollte die Variable mit einem leeren Array gleichstellen
+//variables later purposes
+var selected: Basis; 
+var selectWord: string[] = [];                                               // this empty bracket thing should equate the variable to an empty array
 var previousSentences: number[] = [];
 var countTracker: number = 0;
 var sentenceTracker: number = 0;
+var diffTracker: number = 0;
+
+var easyDiff = document.querySelector(".easy");  
+var midDiff = document.querySelector(".mid");
+var hardDiff = document.querySelector(".hard");
 
 
-
-// folgende Funktion erzeugt eine zufällige Zahl und sendet diese später an die richtige Stelle
+// the following function generates a random number and later on sends it to correct location
 function RandomSentence() {
-    var max: number = 14;
 
-    var random: number = Math.floor(Math.random() * (max + 1));
-    while (previousSentences.includes(random)) {
-        random = Math.floor(Math.random() * (max + 1));
-    }
-    previousSentences.push(random); //keeps track of the number
-    return sentences[random];
+ var max: number = 14;
+ var random: number = Math.floor(Math.random() * (max + 1));
+
+ while (previousSentences.includes(random)) {                             //keeps picking a random number that hasnt been selected yet
+     random = Math.floor(Math.random() * (max + 1));
+ }
+
+ previousSentences.push(random);                                          //keeps track of the picked numbers
+
+ return sentences[random];
 }
 
+
+// following function randomizes the order of the spanish words
 function RandomSpanish(spanish: string[]) {
 
-    let newSpanish = [];
-    for (let i = 0; i < spanish.length; i++) {   //creates a copy of "spanish" and its newSpanish
-        newSpanish[i] = spanish[i];
-    }
+ let newSpanish = [];                                                        // a copy of the original 'spanish'; its to avoid updating the original one 
 
-    for (let i = newSpanish.length - 1; i > 0; i--) {  //swaps items around randomly based on the Fisher–Yates shuffle
-        var rdm = Math.floor(Math.random() * (i + 1));
-        var old = newSpanish[i];
-        newSpanish[i] = newSpanish[rdm];
-        newSpanish[rdm] = old;
-        }
-    return newSpanish;
+ for (let i = 0; i < spanish.length; i++) {   
+     newSpanish[i] = spanish[i];
+ }
+
+ for (let i = newSpanish.length - 1; i > 0; i--) {                          // swaps items around randomly based on the Fisher–Yates shuffle
+     var rdm = Math.floor(Math.random() * (i + 1));
+     var old = newSpanish[i];
+     newSpanish[i] = newSpanish[rdm];
+     newSpanish[rdm] = old;
+ }
+
+ return newSpanish;
     
 
 } 
 
 function Injector() {
 
-    selected = RandomSentence(); 
+ selected = RandomSentence(); 
 
-    document.querySelector(".head").innerHTML = selected.german;
-    console.log(selected);
+ document.querySelector (".point-counter").innerHTML = countTracker.toString() + " Pts." ;      // showing of the total points in html
+ document.querySelector(".head").innerHTML = " \" " + selected.german + " \" ";                 // showing off the germans sentence
 
-    var gridContainer = document.querySelector(".grid-container");
-    var inputBox = document.querySelector(".input-box");
+ var gridContainer = document.querySelector(".grid-container");                                 // variables for convienience
+ var inputBox = document.querySelector(".input-box");
  
-    var rngSpanish = RandomSpanish(selected.spanish);
+ var rngSpanish = RandomSpanish(selected.spanish);
 
-    gridContainer.innerHTML = "";  //entfernt alle elemete aus dem Grid 
-    inputBox.innerHTML = "";       // entfernt alle Übersetzungen
-    selectWord = [];               // leert die Worterkennung für den nächsten Satz
+ gridContainer.innerHTML = "";                                                                  // removes all elements from the grid
+ inputBox.innerHTML = "";                                                                       // removes all translated words
+ selectWord = [];                                                                               // clears the tracker for the next sentence
 
-    for (let index = 0; index < rngSpanish.length; index++) {
-        let element = rngSpanish[index];
+ for (let index = 0; index < rngSpanish.length; index++) {                                      //creating the 'spanish' as grid-items in the grid-container 
+      let element = rngSpanish[index];
         
-        var item: HTMLDivElement = document.createElement("div");
-        item.setAttribute("class", "grid-item");
-        item.innerHTML = element;
-        item.addEventListener("click", function() { OrderChecker(element); });
+      var item: HTMLDivElement = document.createElement("div");
+      item.setAttribute("class", "grid-item");
+      item.innerHTML = element;
+      item.addEventListener("click", function() { OrderChecker(element); });
 
-        gridContainer.appendChild(item);
-    }
+      gridContainer.appendChild(item);                                                           //adds 'item' as a child to grid-container -> puts grid-item into grid-container
+}
 
 }
 
-function OrderChecker(word: string) {
-// index für die korrekte spanische Wort-liste, die gecheckt wird
-// die Wörter werden durch deren numerischen Wert im Array in die input-box im html ausgespuckt
-var nextSpanish: number = selectWord.length;
-console.log(nextSpanish);
-console.log(word);
-console.log(selected.spanish[nextSpanish]);
-console.log(selected);
-if (selected.spanish[nextSpanish] == word) {
+  // index for the propper spanish list of words that gets checked
+  // said words will ne displayed in the html element based on the numerical value within the array
+function OrderChecker(word: string) {                                                                //word = which spanish was clicked on
+
+ var nextSpanish: number = selectWord.length;
+
+ if (selected.spanish[nextSpanish] == word) {                                                         //its checking if the clicked word is the correct one
    var input = document.querySelector(".input-box");
-   input.innerHTML = input.innerHTML  +" "+ word;  //das ist dazu da, nach der richtigen antwort nochmal ein abstand einzubringe
-   selectWord.push(word); //Das ist da um die Auswahl bzw die reihnfolge zu merken
+   input.innerHTML = input.innerHTML  + " " + word;                                                  // this is here to put some space inbetween the correct answers
+   selectWord.push(word);                                                                            // this part is there to remember the order of the selection
    countTracker++;
-}
-else {
+ }
+ else {
   alert("Das war leider falsch. Versuchs erneut! ");
   countTracker--;
 
-}
-if (countTracker < 0) {
+ }
+ if (countTracker < 0) {
     countTracker = 0;
-}
+ }
 
-document.querySelector (".point-counter").innerHTML = countTracker.toString() + " Pts." ;
+ document.querySelector (".point-counter").innerHTML = countTracker.toString() + " Pts." ;
 
-var easyDiff = document.querySelector(".easy");  
-var midDiff = document.querySelector(".mid");
-var hardDiff = document.querySelector(".hard");
-
-if ( selectWord.length == selected.spanish.length) {   //= 
+ if ( selectWord.length == selected.spanish.length) {                                               // if the amount of correct words coressponds with the total length, the sentences completed
     sentenceTracker++;
 
-    if (sentenceTracker == 5) {
-        if (confirm("Glückwunsch! Die Punktzahl: " + countTracker + ". Du hast den ersten Kurs geschafft! Möchtest du fortfahren?")) {
+    if (sentenceTracker == 5 && diffTracker == 0) {                                                // if the amount of completed sentences & difficulty align with the condition, a confirm prompt will ask to continue
+     if (confirm("Glückwunsch! Die Punktzahl: " + countTracker + ". Du hast den ersten Kurs geschafft! Möchtest du fortfahren?")) {
          midDiff.setAttribute("class", "diff mid current");
          easyDiff.setAttribute("class", "diff easy");
          countTracker = 0;
          Injector();
-        } 
-     }
+     } 
+    }
      
-     else if (sentenceTracker == 10) {
-         if (confirm("Glückwunsch! Die Punktzahl: " + countTracker + ". Du hast den zweiten Kurs geschafft! Möchtest du fortfahren?")) {
-             hardDiff.setAttribute("class", "diff hard current");
-             midDiff.setAttribute("class", "diff mid");
-             countTracker = 0;
-             Injector();
-            } 
-     }
-     
-     else if (sentenceTracker == 15) {
-         alert("Glückwunsch! Die Punktzahl: " + countTracker + ". Du hast den ganzen Kurs geschafft!");
+     else if (sentenceTracker == 10 && diffTracker == 1) {
+     if (confirm("Glückwunsch! Die Punktzahl: " + countTracker + ". Du hast den zweiten Kurs geschafft! Möchtest du fortfahren?")) {
          hardDiff.setAttribute("class", "diff hard current");
-     }
+         midDiff.setAttribute("class", "diff mid");
+         countTracker = 0;
+         Injector();
+         } 
+    }
      
-     else {   
-             Injector();
-     }
+    else if (sentenceTracker == 15) {
+         alert("Glückwunsch! Die Punktzahl: " + countTracker + ". Du hast den dritten Kurs geschafft!");
+         hardDiff.setAttribute("class", "diff hard current");
+    }
+     
+    else {   
+         Injector();
+    }
+}
 }
 
+// a function to enable difficulty selection
+function DiffSelect() { 
+
+ easyDiff = document.querySelector(".easy");  
+ midDiff = document.querySelector(".mid");
+ hardDiff = document.querySelector(".hard");
+
+ easyDiff.addEventListener("click", function() { SetDiff(0); });
+ midDiff.addEventListener("click", function() { SetDiff(1); });
+ hardDiff.addEventListener("click", function() { SetDiff(2); });
+
+}
+
+function SetDiff(track: number) {                                         //track = selected difficulty
+    diffTracker = track;
+
+    previousSentences = [];
+    sentenceTracker = 0;
+    countTracker = 0;
+    
+    if (track == 0) {                                                     // adjusting styling after selection
+             easyDiff.setAttribute("class", "diff easy current");
+             midDiff.setAttribute("class", "diff mid");
+             hardDiff.setAttribute("class", "diff hard");
+         }
+         
+     else if (track == 1) {
+            
+        easyDiff.setAttribute("class", "diff easy");
+        midDiff.setAttribute("class", "diff mid current");
+        hardDiff.setAttribute("class", "diff hard");             
+         }
+         
+         else if (track == 2) {    
+                       
+        easyDiff.setAttribute("class", "diff easy");
+        midDiff.setAttribute("class", "diff mid");
+        hardDiff.setAttribute("class", "diff hard current"); 
+         }
+          
+    Injector();
+    document.querySelector (".point-counter").innerHTML = countTracker.toString() + " Pts." ;
+         
+    
 }
